dadb6204bc2e7cd391d917b8b33e306a1f1a7945
zouqianli <1004248149@qq.com>
2017-05-03 01:58:38 +0800

    Initial Commit

---
+++ News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  News
+//
+//  Created by 邹前立 on 2017/5/3.
+//  Copyright © 2017年 邹前立. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ News/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  News
+//
+//  Created by 邹前立 on 2017/5/3.
+//  Copyright © 2017年 邹前立. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
